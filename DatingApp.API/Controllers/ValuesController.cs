5d35f19507a97280a737c3233a14e29a3f561fe5
Alison Agnelo Fernandes <alisonline@gmail.com>
2020-04-22 16:55:49 +0100

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase {
+        private readonly DataContext context;
+        public ValuesController(DataContext context) {
+            this.context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues() {
+            var values = await context.Values.ToArrayAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id) {
+            var value = await context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value) { }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
